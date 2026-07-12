f974065ed272a73e7707a008aaed9169deec9d8d
swatipathak770 <pathakswati276@gmail.com>
2026-07-12 21:17:42 +0530

    feat: initialize Spring Boot backend and User entity

---
+++ src/main/java/com/nexcart/entity/Role.java
@@ -0,0 +1,7 @@
+
+package com.nexcart.entity;
+
+public enum Role {
+    CUSTOMER,
+    ADMIN
+}
---
+++ src/main/java/com/nexcart/entity/User.java
@@ -0,0 +1,73 @@
+package com.nexcart.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Full name is required")
+    @Column(nullable = false)
+    private String fullName;
+
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    private String profileImage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean enabled = false;
+
+    @Column(nullable = false)
+    private Boolean accountNonLocked = true;
+
+    @Column(nullable = false)
+    private Boolean accountNonExpired = true;
+
+    @Column(nullable = false)
+    private Boolean credentialsNonExpired = true;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
